1c60c5194cfdb9fccd82cc2897cf161e18fb8535
Munra <Munra@BABY>
2022-09-19 10:12:53 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = new int[25];
+        Random rnd = new Random();
+        long sum = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(-10, 25);
+            sum = sum + array[i];
+
+            Console.WriteLine("Сумма = {0}", sum);
+        }
+
+        
+    }
+}
+
